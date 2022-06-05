b02e33e55e2ff0bf3b9f50b9698b27b485970828
arhamsc <shaik.arham@gmail.com>
2022-06-05 21:49:34 +0530

    deployed server and client
    Server on heroku and Client on firebase

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5500' });
+const API = axios.create({
+    baseURL: 'https://memoriesmernarham.herokuapp.com',
+});
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')) {
--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,24 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './redux/reducers';
+import { initializeApp } from 'firebase/app';
 
 import App from './App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
+const firebaseConfig = {
+    apiKey: 'AIzaSyDvGHUQ3wWPs9gX5wfkrNZUvc2bQrRX2SA',
+    authDomain: 'memories-mern-418ae.firebaseapp.com',
+    projectId: 'memories-mern-418ae',
+    storageBucket: 'memories-mern-418ae.appspot.com',
+    messagingSenderId: '977005385259',
+    appId: '1:977005385259:web:49f3b8e26348d4e824bc6e',
+    measurementId: 'G-FFGFFWJ2DK',
+};
+
+initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
